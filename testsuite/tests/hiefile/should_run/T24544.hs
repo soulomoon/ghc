5f7d8fca39c2b78e800a13777582bfe86cf4431d
Patrick <fwy996602672@gmail.com>
2024-06-10 02:37:09 +0800

    fix test

--- testsuite/tests/hiefile/should_run/T24544.hs
+++ testsuite/tests/hiefile/should_run/T24544.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE TypeFamilies #-}
-
 module Main where
 
 import TestUtils
-import qualified Data.Map.Strict as Map
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 import Data.Either
 import Data.Maybe
+import Data.Bifunctor (first)
 import GHC.Plugins (moduleNameString, nameStableString, nameOccName, occNameString, isDerivedOccName)
 import GHC.Iface.Ext.Types
-
 -- data EntityInfo
 --   =
 --   EntityVariable
@@ -39,15 +39,12 @@ data RecordFoo = RecordFoo { recordFoo :: Int }
 points :: [(Int,Int)]
 points = [(16,1), (18,9), (20,1), (22,6), (24,6), (26,7), (28,2), (30,9), (32,13), (33,13), (35,6), (37,30)]
 
-getIdentifierEntityInfo :: HieFile -> Identifier -> S.Set EntityInfo
-getIdentifierEntityInfo hf ident = M.findWithDefault S.empty ident (hie_entity_infos hf)
-
 isDerived :: (Identifier, a) -> Bool
 isDerived ((Right name), _) = not $ isDerivedOccName (nameOccName name)
 isDerived ((Left _), _) = True
 main = do
   (df, hf) <- readTestHie "T24544.hie"
   let asts = fmap (fromMaybe (error "nothing") . selectPoint hf) points
-      idents = concatMap (Map.toList . sourcedNodeIdents . sourcedNodeInfo) asts
-      names = map (\(x, y) -> (either moduleNameString  (occNameString . nameOccName) x, getIdentifierEntityInfo hf y)) $ filter isDerived $ idents
+      idents = concatMap (M.toList . sourcedNodeIdents . sourcedNodeInfo) asts
+      names = map (\(x, _) -> (either moduleNameString  (occNameString . nameOccName) x, getIdentifierEntityInfo hf x)) idents
   mapM_ (print) names
